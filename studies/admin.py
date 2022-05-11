c965d17a19c30e6a5a5dcdd2b67a1c46d00325c1
minyoungci <dbssus123@gmail.com>
2022-05-11 18:18:35 +0900

    reset

--- studies/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-@admin.register(models.Study)
-class StudyAdmin(admin.ModelAdmin):
-    pass
--- users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-@admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-    pass
