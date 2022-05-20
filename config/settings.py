b66ca28637c0c3464f35ae34c2515ea1eac094f2
minyoungci <dbssus123@gmail.com>
2022-05-20 18:09:40 +0900

    html css 적용 완료

--- config/settings.py
+++ config/settings.py
@@ -58,10 +58,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "config.urls"
 
+import os
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
